b523cc00b0bbe459fe523da3afa369de1f3c1f71
Floris <floris__2@hotmail.com>
2021-12-31 14:39:24 +0100

    add/fill object based on API data

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,20 @@
-let api = "https://pokeapi.co/api/v2/pokemon/";
-let pokemon = api;
+const fetchPokemon = () => {
+    const api = "https://pokeapi.co/api/v2/pokemon/";
+    for(id = 1; id <4; id++) { /*Loop to fill id to end of api url*/
+        apiAndId = api + id + "/";
+        fetch(apiAndId)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data);
+            const pokemon = { /*creating object and fill with api data*/
+                name: data.name,
+                id: data.id,
+                img: data.sprites.front_default,
+                type: data.types[0].type.name
+            };
+            console.log(pokemon);
+        });
+    };
+};
 
-for(let id = 1; id < 20; id++) {
-    pokemon = api + id + "/";
-    fetch(pokemon)
-    .then(response => response.json())
-    .then(data => console.log(data.name));
-}
-
-
-
-
-
+fetchPokemon();
