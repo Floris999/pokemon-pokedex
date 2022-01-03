c9595a73011b5cdc987eb6b79da9521a7ec5c63c
Lee Ravenberg <ravenberg@users.noreply.github.com>
2022-01-03 15:56:42 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,22 @@
 const fetchPokemon = () => {
     const api = "https://pokeapi.co/api/v2/pokemon/";
+    const pokemon = [];
     for(id = 1; id <4; id++) { /*Loop to fill id to end of api url*/
         apiAndId = api + id + "/";
         fetch(apiAndId)
         .then(response => response.json())
         .then((data) => {
             console.log(data); /*api data based on ID*/
-            const pokemon = { /*creating object and fill with api data*/
+            pokemon.push({ /*creating object and fill with api data*/
                 name: data.name,
                 id: data.id,
                 img: data.sprites.front_default,
                 type: data.types[0].type.name
-            };
+            });
             console.log(pokemon);
         });
     };
+    return pokemon;
 };
 
 fetchPokemon();
