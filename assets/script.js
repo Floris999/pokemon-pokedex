427d5542492656441977532f2f16a39c5490f0ae
Floris <54399085+Floris999@users.noreply.github.com>
2022-01-13 15:47:19 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
 const fetchPokemon = () => {
-    const api = "https://pokeapi.co/api/v2/pokemon/";
-    for(id = 1; id <4; id++) { /*Loop to fill id to end of api url*/
-        apiAndId = api + id + "/";
-        fetch(apiAndId)
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data); /*api data based on ID*/
-            const pokemon = { /*creating object and fill with api data*/
-                name: data.name,
-                id: data.id,
-                img: data.sprites.front_default,
-                type: data.types[0].type.name
-            };
-            console.log(pokemon);
-        });
-    };
-};
+  const promises = [];
+  let url = "https://pokeapi.co/api/v2/pokemon/";
+  for(id = 1; id <150; id++) { 
+        apiAndId = url + id + "/";
+       promises.push(fetch(apiAndId).then((res) => res.json()));
+    }
+Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            id: result.id,
+            img: result.sprites.front_default,
+            type: result.types[0].type.name
+        }));
+        displayPokemon(pokemon);
+    });
+  }
+
+
+const displayPokemon = (pokemon) => {
+  console.log(pokemon);
+  pokemon.map(split => {
+    document.getElementById("pokemonName").innerHTML = `<h1>${pokemon.name}</h1>`;
+  })
+
+}
 
 fetchPokemon();
